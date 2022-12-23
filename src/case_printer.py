d90be54d1cf41871e290f9333ca3222e2baca42a
Gal <galwiernik@gmail.com>
2022-12-23 13:44:47 +0200

    now with centred case (aligned the outer hull)

--- src/case_printer.py
+++ src/case_printer.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy.linalg import norm
 import copy
 import os
+import math
 
 
 class CasePrinter(object):
@@ -12,29 +13,42 @@ class CasePrinter(object):
         self._mesh = pymesh.load_mesh(mesh_path)
         # self._mesh = self.fix_mesh(self._mesh)
 
-    def create_case(self) -> pymesh.Mesh:
+    def create_case(self, thickness=1) -> pymesh.Mesh:
         # Compute the convex hull
         print("Comupting convex hull")
         # This might be useful someday: pymesh.compute_outer_hull(mesh)
         hull = pymesh.convex_hull(self._mesh)
 
-        sphere = pymesh.generate_icosphere(radius=1,
-                                           center=(0, 0, 0),
-                                           refinement_order=self._REFINEMENT_ORDER)
-
-        bigger_hull = pymesh.form_mesh(hull.vertices * 1.1, hull.faces)
+        bigger_hull = self.get_outer_case(hull, thickness=thickness)
 
         diff = pymesh.boolean(bigger_hull, hull, operation='difference') # There is some adjutment needed with some shifting of the bigger hull..
         top_half, bottom_half = self.split_mesh_in_two(diff)
 
         return top_half, bottom_half
 
+    def get_outer_case(self, mesh, thickness):
+        """
+        Option 1: Scale
+        Option 2: miknowski
+
+        sphere = pymesh.generate_icosphere(radius=1,
+                                           center=(0, 0, 0),
+                                           refinement_order=self._REFINEMENT_ORDER)
+        sphere_polyline = self.get_sphere_polyline(radius=thickness)
+        box = pymesh.generate_box_mesh((0,0,0), (1,1,1))
+        #outer_case = pymesh.minkowski_sum(mesh, box)
+        """
+        center = (mesh.bbox[0] + mesh.bbox[1]) / 2
+        outer_case = pymesh.form_mesh(mesh.vertices * 1.1 - (center * 0.1), mesh.faces)
+        
+        return outer_case
+
     def split_mesh_in_two(self, mesh: pymesh.Mesh):
         """
         1. Split the bouding box into two bounding boxes on top of each other.
         2. Intersect given mesh with a each half of the bouding box
         """
-        mesh.bbox
+        
         bottom, top = mesh.bbox[0], mesh.bbox[1]
         mid_height = (bottom[2] + top[2]) / 2
 
@@ -46,6 +60,20 @@ class CasePrinter(object):
 
         return top_half, bottom_half
     
+    def get_sphere_polyline(self, radius, points=6):
+        n = points
+        x, y, z = [], [], []
+        # Calculate the points on the sphere
+        for i in range(n):
+            for j in range(n):
+                # Calculate the coordinates
+                x.append(radius * math.sin(math.pi * i / n) * math.cos(2 * math.pi * j / n))
+                y.append(radius * math.sin(math.pi * i / n) * math.sin(2 * math.pi * j / n))
+                z.append(radius * math.cos(math.pi * i / n))
+
+        polyline = np.column_stack((x, y, z))
+        return polyline
+
     @staticmethod
     def save_mesh_to_stl(mesh, output_path):
         print("Saving mesh")
@@ -66,19 +94,19 @@ class CasePrinter(object):
         # Show the plot
         mesh.plot()
 
-    def display_two_meshes(self, mesh1, mesh2):
+    def display_two_meshes(self, mesh1, mesh2, show_edges=False):
         plotter = pyvista.Plotter(shape=(1, 2))
 
         # Note that the (0, 0) location is active by default
         # load and plot an airplane on the left half of the screen
         plotter.add_text("Top Half", font_size=30)
-        plotter.add_mesh(self._pymesh_to_pyvista(mesh1), show_edges=False)
+        plotter.add_mesh(self._pymesh_to_pyvista(mesh1), show_edges=show_edges)
 
         # load and plot the uniform data example on the right-hand side
         plotter.subplot(0, 1)
         plotter.add_text("Bottom half\n", font_size=30)
-        plotter.add_mesh(self._pymesh_to_pyvista(mesh2), show_edges=True)
-        plotter.link_views()
+        plotter.add_mesh(self._pymesh_to_pyvista(mesh2), show_edges=show_edges)
+        # plotter.link_views()
         # Display the window
         plotter.show()
 
